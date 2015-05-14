b3e2f67b62d88265a708db9cca3e52dc8819f42e
S <s>
2015-05-14 18:34:46 -0500

    {"type":"TI", "runs":"1"}

--- Breakout.java
+++ Breakout.java
@@ -132,7 +132,7 @@ public class Breakout extends GraphicsProgram {
 
 	private void createPaddle()
 	{
-		paddle = new GRect(WIDTH - PADDLE_WIDTH / 2, PADDLE_Y_OFFSET, PADDLE_WIDTH, HEIGHT - PADDLE_HEIGHT);
+		paddle = new GRect(WIDTH - PADDLE_WIDTH / 2, HEIGHT - PADDLE_Y_OFFSET, PADDLE_WIDTH, PADDLE_HEIGHT);
 		add(paddle);
 	}
 }
