28deeb3822a3167d3eb9642318d3e8afeb352535
S <s>
2015-05-14 20:31:30 -0500

    {"type":"TI", "runs":"0"}

--- Breakout.java
+++ Breakout.java
@@ -210,5 +210,23 @@ public class Breakout extends GraphicsProgram {
 	{
 		double xPos = ball.getX();
 		double yPos = ball.getY();
+		
+		if(getElementAt(xPos, yPos))
+		{
+			
+		}
+		else if(getElementAt(xPos, yPos + 2 * BALL_RADIUS))
+		{
+			
+		}
+		else if(getElementAt(xPos + 2 * BALL_RADIUS, yPos))
+		{
+			
+		}
+		else if(getElementAt(xPos + 2 * BALL_RADIUS, yPos + 2 * BALL_RADIUS))
+		{
+			
+		}
+		}
 	}
 }
